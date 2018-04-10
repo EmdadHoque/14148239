486add5cc605e58981dea47f2983844e0e206187
EmdadHoque <38159273+EmdadHoque@users.noreply.github.com>
2018-04-10 17:31:03 +0100

    Add files via upload

---
+++ MobileApp/MobileApp/DraggedCarView.swift
@@ -0,0 +1,28 @@
+//
+//  DraggedCarView.swift
+//  MobileApp
+//
+//  Created by eh15aaa on 10/04/2018.
+//  Copyright © 2018 eh15aaa. All rights reserved.
+//
+
+import UIKit
+
+class DraggedCarView: UIImageView {
+
+    var startLocation: CGPoint?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    { startLocation = touches.first?.location(in:self) }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    
+    {
+        let currentLocation = touches.first?.location(in: self)
+        let dx = currentLocation!.x - startLocation!.x
+        let dy = currentLocation!.y - startLocation!.y
+        
+        self.center = CGPoint(x: self.center.x+dx, y: self.center.y+dy)
+    }
+    
+
+}
