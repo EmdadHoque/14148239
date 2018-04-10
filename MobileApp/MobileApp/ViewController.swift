d11c2c9a78ef36df1543ed63d444988bd47ecc36
EmdadHoque <38159273+EmdadHoque@users.noreply.github.com>
2018-04-10 23:48:30 +0100

    Add files via upload

--- MobileApp/MobileApp/ViewController.swift
+++ MobileApp/MobileApp/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
     
     // ROAD image view
     @IBOutlet var roadImage: UIImageView!
-    
+    // WALKING image view
+    @IBOutlet var walkingImage: UIImageView!
     
     
     
@@ -42,6 +43,40 @@ class ViewController: UIViewController {
         }
         roadImage.animationImages = images
         roadImage.startAnimating()
+        
+        //Animation of walking
+        var walkingArray: [UIImage]!
+        
+        walkingArray = [UIImage(named: "walking1")!,
+                      UIImage(named: "walking2")!,
+                      UIImage(named: "walking3")!,
+                      UIImage(named: "walking4")!,
+                      UIImage(named: "walking5")!,
+                      UIImage(named: "walking6")!,
+                      UIImage(named: "walking7")!,
+                      UIImage(named: "walking8")!,
+                      UIImage(named: "walking9")!,
+                      UIImage(named: "walking10")!,
+                      UIImage(named: "walking11")!,
+                      UIImage(named: "walking12")!,
+                      UIImage(named: "walking13")!]
+        
+        walkingImage.image = UIImage.animatedImage(with: walkingArray, duration: 1)
+        
+        walkingImage.backgroundColor = UIColor.clear
+        
+        
+        
+        // Animation of walking across road
+        UIView.animate(withDuration: 2, delay: 4.0, options: [UIViewAnimationOptions.repeat, .curveLinear], animations:
+            {
+                
+                self.walkingImage.center.x += self.view.bounds.width
+                
+        }, completion: nil
+        )
+        
+        
     }
     
     
@@ -53,4 +88,7 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
 
