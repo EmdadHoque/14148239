640ea461c59feaa3ccb182bde25ec35f4ccbb0e6
EmdadHoque <38159273+EmdadHoque@users.noreply.github.com>
2018-04-19 23:12:04 +0100

    Add files via upload

--- MobileApp/MobileApp/ViewController.swift
+++ MobileApp/MobileApp/ViewController.swift
@@ -14,21 +14,22 @@ protocol subviewDelegate {
 
 
 //////////////////////////////////////////
+
 class ViewController: UIViewController, subviewDelegate {
     func changeSomething() {
         collisionBehavior.addBoundary(withIdentifier: "barrier" as
             NSCopying, for: UIBezierPath(rect: carView0.frame))
     }
 
-    let gameover = UIImageView(image: nil)
+  
     
     
   
     
+    var fallcars = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     var dynamicAnimator: UIDynamicAnimator!
     var dynamicItemBehavior: UIDynamicItemBehavior!
     var collisionBehavior: UICollisionBehavior!
-    var collisionBehavior1: UICollisionBehavior!
     var gravityBehavior: UIGravityBehavior!
 
     
@@ -44,40 +45,8 @@ class ViewController: UIViewController, subviewDelegate {
     // CAR0 image view
     @IBOutlet weak var carView0: DraggedCarView!
     
-    // BLUE CAR1-CAR3 image view
-    @IBOutlet weak var carView1: UIImageView!
-    
-    
-  
-    
-    // RED CAR4-CAR5 image view
-    @IBOutlet weak var carView2: UIImageView!
-
-    
-    // CYAN CAR6-CAR8 image view
-  
-    
-    // YELLOW CAR9-CAR11 image view
-  
-    
-    // ORANGE CAR12-CAR14 image view
-    
-    
-    // GREEN CAR15-CAR20 image view
-  
-    
-    
     
    
-    
- 
-    
-    
-    
-    
-    
-    
-    
     
     
 //////////////////////////////////////////
@@ -95,31 +64,15 @@ class ViewController: UIViewController, subviewDelegate {
     
         
         
-        let randomNumber1 = arc4random_uniform(0) + 200 // speed y
-        let randomNumberx1 = arc4random_uniform(198) + 40  // location x
-        self.carView1.frame = CGRect (x: Int(randomNumberx1), y: -200, width: 25, height: 50)
-        
-        
-        let randomNumberx2 = arc4random_uniform(198) + 40  // location x
-        self.carView2.frame = CGRect (x: Int(randomNumberx2), y: -200, width: 25, height: 50)
-        
         
 
         // Animation for cars
-        
-        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
-        collisionBehavior = UICollisionBehavior(items: [carView1, carView2])
-        collisionBehavior.translatesReferenceBoundsIntoBoundary = false
-        dynamicAnimator.addBehavior(collisionBehavior)
-        dynamicItemBehavior = UIDynamicItemBehavior(items: [carView1, carView2])
+    
         
         
-        
-        self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: 0, y: Int(randomNumber1)), for: carView1)
-        self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: 0, y: Int(randomNumber1)), for: carView2)
+       
         
       
-        dynamicAnimator.addBehavior(dynamicItemBehavior)
         
 
         
@@ -211,19 +164,83 @@ class ViewController: UIViewController, subviewDelegate {
         DispatchQueue.main.asyncAfter(deadline: when)
         {
             
-            
-            self.gameover.image = UIImage(named:"game_over.png")
-            self.gameover.frame = UIScreen.main.bounds
-            self.view.addSubview(self.gameover)
-            self.view.bringSubview(toFront: self.gameover)
-            
+          
             
             
            
            
             
         }
+        
+        
+        self.dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        self.dynamicItemBehavior = UIDynamicItemBehavior(items: [])
+        
+        
+        // Car Generator
+        
+        for obstacle in 0...19{
+            
+            let wait1 = Double(self.fallcars[obstacle])
+            let wait = DispatchTime.now() + wait1
+            
+            DispatchQueue.main.asyncAfter(deadline: wait) {
+                
+                let carColour = arc4random_uniform(6)
+                let RandomCar = UIImageView(image: nil)
+                
+                switch carColour
+                {
+                    
+                    case 1 : RandomCar.image = UIImage(named: "car1.png")
+                    case 2:  RandomCar.image = UIImage(named: "car2.png")
+                    case 3 : RandomCar.image = UIImage(named: "car3.png")
+                    case 4 : RandomCar.image = UIImage(named: "car4.png")
+                    case 5 : RandomCar.image = UIImage(named: "car5.png")
+                    case 6 : RandomCar.image = UIImage(named: "car6.png")
+                    default: RandomCar.image = UIImage(named: "car1.png")
+                }
+                
+                
+                
+                let randomNx2 = arc4random_uniform(201) + 40
+                RandomCar.frame = CGRect(x: Int(randomNx2), y: 0, width: 30, height: 50)
+                self.view.addSubview(RandomCar)
+                self.view.bringSubview(toFront: RandomCar)
+                
+                
+                
+                // Falling Cars
+                
+                let randomN2 = arc4random_uniform(75) + 200
+                let randomN3 = arc4random_uniform(100) + 500
+                
+                
+                self.dynamicItemBehavior.addItem(RandomCar)
+                self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: 0, y: Int(randomN3)), for: RandomCar)
+                
+                // Collision of Cars
+                
+                self.dynamicAnimator.addBehavior(self.dynamicItemBehavior)
+                self.collisionBehavior = UICollisionBehavior(items: [RandomCar])
+                self.collisionBehavior.translatesReferenceBoundsIntoBoundary = false
+                self.dynamicAnimator.addBehavior(self.collisionBehavior)
+                self.dynamicItemBehavior.elasticity = 0-1
+            }
+            
+            let when = DispatchTime.now() + 5
+            
+            DispatchQueue.main.asyncAfter(deadline: when) {
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "gameOverScreen") as! GameOverPage
+                self.present(vc, animated: false, completion: nil)
+            }
+            
         }
+    }
+
+    
+        
         
     
     
